583f0ceae7a982c6003bbd8684ba15be6f046be6
Alfonso Sarmiento <alfitero6@gmail.com>
2025-08-09 13:38:27 -0300

    Client creation made

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,9 +1,64 @@
 const { request, response } = require("express");
+const bcrypt = require('bcryptjs');
+const client = require("../models/client");
 
 
-//TODO: CREATE A USER
-const createUser = (req = request, res = response) => {
-  res.send("User created successfully", 200);
+//TODO: VALIDATE THE FIELDS
+const createUser = async(req = request, res = response) => {
+  
+
+  try {
+    
+    const { 
+      userName, 
+      email, 
+      name, 
+      surname, 
+      password,
+      imgProfile } = req.body;
+    
+      // Validate required fields
+  
+      // Encrypt the password
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+  
+  
+  
+      //Create a new user
+      const newUser = {
+        userName,
+        email,
+        name,
+        surname,
+        password: hashedPassword,
+        imgProfile,
+        role: 'client', // Default role
+        isMember: false, // Default membership status
+        registrationDate: new Date() // Current date as registration date
+      };
+    
+      const user = await client.create(newUser);
+    
+      return res.status(201).json({
+        message: 'User created successfully',
+        user
+      });
+  
+    } catch (error) {
+
+      return res.status(500).json({
+        message: 'Error creating user',
+        error: error.message
+      });
+    
+  }
+
+
+
+
+
+
 }
 
 
