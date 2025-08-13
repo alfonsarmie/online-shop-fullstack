d0915f8f3c15b23208ad3f9283306fe1cec56605
Alfonso Sarmiento <alfitero6@gmail.com>
2025-08-12 22:10:30 -0300

    User deletion with admin authentication implemented

--- backend/middlewares/validate-jwt.js
+++ backend/middlewares/validate-jwt.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
+const user = require('../models/user');
 
 
-const validateJWT = (req, res, next) => {
+const validateJWT = async(req, res, next) => {
     
     const token = req.header('x-token'); //This is the name of the header frontend will send the token
     if (!token) {
@@ -13,15 +14,35 @@ const validateJWT = (req, res, next) => {
     try {
         
         const { userId } = jwt.verify(token, process.env.JWT_SECRET);
-        
         const idToDelete = req.params.id;
         
-        if (userId !== parseInt(idToDelete, 10)) {
+        console.log();
+        
+
+        const userToValidate = await user.findByPk(userId);
+
+        if (userToValidate.role !== 'admin') {
             return res.status(403).json({
                 message: 'You do not have permission to perform this action'
             });
         }
-        
+
+        // Verify if user is not already deleted or exists
+        if(!userToValidate || userToValidate.status === 'deleted') {
+            return res.status(404).json({
+                message: 'User not found or already deleted'
+            });
+        }
+
+
+        // Only admin can delete any user, non-admin can only delete themselves
+        if (userId !== parseInt(idToDelete, 10) && userToValidate.role !== 'admin') {
+            return res.status(403).json({
+                message: 'You do not have permission to perform this action'
+            });
+        }
+
+
 
         next();
     
