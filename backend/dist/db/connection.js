5bddb807f979a74fbf88e78e21f0217c33fd6e18
nicotg <nicolatarrio95@gmail.com>
2025-09-05 09:56:58 -0300

    Database connection fixed

--- backend/dist/db/connection.js
+++ backend/dist/db/connection.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDB = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const sequelize_1 = require("sequelize");
 const db = new sequelize_1.Sequelize(process.env.DB_NAME || 'online_shop_fullstack_rowing', process.env.DB_USERNAME || '', process.env.DB_PASSWORD || '', {
     host: process.env.DB_HOST || 'localhost',
